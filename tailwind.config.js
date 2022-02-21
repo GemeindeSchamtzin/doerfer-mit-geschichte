e5e01ad3035cf0a537b8cf81cc833716aedc9987
schafevormfenster <jan@schafe-vorm-fenster.de>
2022-02-21 14:39:18 +0100

    Initial version.

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+  theme: {
+    fontFamily: {
+      sans: ['serif'],
+    },
+    fontSize: {
+      s: '1rem',
+      m: '1.3rem',
+      l: '2.5rem',
+      xl: '2.7rem',
+    },
+    extend: {
+      colors: {
+        primary: '#75010d',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+};
